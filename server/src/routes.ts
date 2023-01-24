56143d1a9c7d4382bc5a92062e5b4ab6327a7748
Thomas Aleknovic <aleknovic70@gmail.com>
2023-01-24 09:08:44 -0300

    feat: Add new routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -9,24 +9,132 @@ export async function appRoutes(app: FastifyInstance) {
       title: z.string(),
       weekDays: z.array(z.number().min(0).max(6)),
     });
-    const { title, weekDays } = createHabitBody.parse(req.body);
+    try {
+      const { title, weekDays } = createHabitBody.parse(req.body);
 
-    const today = dayjs().startOf("day").toDate();
+      const today = dayjs().startOf("day").toDate();
 
-    await prisma.habit.create({
-      data: {
-        title,
-        created_at: today,
-        weekDays: {
-          create: weekDays.map((day) => {
-            return {
-              week_day: day,
-            };
-          }),
+      await prisma.habit.create({
+        data: {
+          title,
+          created_at: today,
+          weekDays: {
+            create: weekDays.map((day) => {
+              return {
+                week_day: day,
+              };
+            }),
+          },
         },
-      },
+      });
+
+      res.send("Habit Created!");
+    } catch (err) {
+      if (err instanceof z.ZodError) {
+        res.send(err.issues);
+      }
+    }
+  });
+
+  app.get("/day", async (req, res) => {
+    const getDayParams = z.object({
+      date: z.coerce.date(),
     });
 
-    res.send("Habit Created!");
+    try {
+      const { date } = getDayParams.parse(req.query);
+      const parsedDate = dayjs(date).startOf("day");
+      const weekDay = parsedDate.get("day");
+
+      const possibleHabits = await prisma.habit.findMany({
+        where: {
+          created_at: {
+            lte: date,
+          },
+          weekDays: {
+            some: {
+              week_day: weekDay,
+            },
+          },
+        },
+      });
+
+      const day = await prisma.day.findUnique({
+        where: {
+          date: parsedDate.toDate(),
+        },
+        include: {
+          dayHabits: true,
+        },
+      });
+
+      const completedHabits = day?.dayHabits.map((dayHabit) => dayHabit.id);
+
+      return { possibleHabits, completedHabits };
+    } catch (err) {
+      if (err instanceof z.ZodError) {
+        res.send(err.issues);
+      }
+    }
   });
+
+  app.post("/habits/:id/toggle", async (req, res) => {
+    const toggleHabitParams = z.object({
+      id: z.string().uuid(),
+    });
+
+    try {
+      const { id } = toggleHabitParams.parse(req.params);
+      const today = dayjs().startOf("day").toDate();
+
+      // conferir se existe o dia na tabela day
+      let day = await prisma.day.findUnique({
+        where: {
+          date: today,
+        },
+      });
+      // se não existir o dia, cria o dia para poder completar o hábito
+      if (!day) {
+        day = await prisma.day.create({
+          data: {
+            date: today,
+          },
+        });
+      }
+
+      const dayHabit = await prisma.dayHabit.findUnique({
+        where: {
+          day_id_habit_id: {
+            day_id: day.id,
+            habit_id: id,
+          },
+        },
+      });
+
+      if (dayHabit) {
+        // se o hábito já existir, removemos ele da lista de completos
+        await prisma.dayHabit.delete({
+          where: {
+            id: dayHabit.id,
+          },
+        });
+      } else {
+        // caso ainda não exista, completamos o hábito
+        await prisma.dayHabit.create({
+          data: {
+            day_id: day.id,
+            habit_id: id,
+          },
+        });
+      }
+
+      res.send(`Changes in habit ${id}`);
+    } catch (err) {
+      if (err instanceof z.ZodError) {
+        res.send(err.issues);
+      }
+    }
+  });
+
+  app.get("/summary", async (req, res) => {});
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,5 @@
 import Fastify from "fastify";
-import cors from '@fastify/cors'
-import { prisma } from "./lib/prisma";
+import cors from "@fastify/cors";
 import { appRoutes } from "./routes";
 
 const app = Fastify();
@@ -8,8 +7,12 @@ const app = Fastify();
 app.register(cors);
 app.register(appRoutes);
 
-app.listen({
-    port: 3333
-}).then(() => {
-    console.log("Server running on http://localhost:3333")
-})
+const start = async () => {
+  try {
+    await app.listen({ port: 3333 });
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+start();
