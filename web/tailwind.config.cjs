65074fc8e68186512f091e0d1499a3fc86a2ec49
Thomas Aleknovic <aleknovic70@gmail.com>
2023-01-24 18:13:40 -0300

    feat: add visual features

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.tsx',
-    './index.html'
-  ],
+  content: ["./src/**/*.tsx", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#09090A",
+        roxo: "#8B5CF6",
+      },
+      gridTemplateRows: {
+        7: "repeat(7, minmax(0, 1fr))",
+      },
+    },
   },
   plugins: [],
-}
+};
