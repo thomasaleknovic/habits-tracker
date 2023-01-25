873faec5689e0fd05acae9104a2badf354cb91a1
Thomas Aleknovic <aleknovic70@gmail.com>
2023-01-25 14:14:52 -0300

    feat: Add habits modals

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,12 +1,36 @@
-import React from "react";
+import * as Popover from "@radix-ui/react-popover";
+import { ProgressBar } from "./ProgressBar";
+import clsx from "clsx";
 
-// interface HabitProps {
-//   completed: number
-// }
+interface HabitProps {
+  amount: number;
+  completed: number;
+}
 
-function HabitDay() {
+function HabitDay({ completed, amount }: HabitProps) {
+  const completedPercentage = Math.round((completed / amount) * 100);
   return (
-    <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg "></div>
+    <Popover.Root>
+      <Popover.Trigger
+        className={clsx(
+          "w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg ",
+          {
+            "bg-violet-500": completedPercentage >= 80,
+          }
+        )}
+      />
+
+      <Popover.Portal>
+        <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
+          <span className="font-semibold text-zinc-400">Quarta-feira</span>
+          <span className="mt-1 font-extrabold text-3xl leading-tight">
+            25/01
+          </span>
+          <ProgressBar progress={completedPercentage} />
+          <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
   );
 }
 
---
+++ web/src/components/ProgressBar.tsx
@@ -0,0 +1,17 @@
+interface ProgressBarProps {
+  progress: number;
+}
+
+export function ProgressBar(props: ProgressBarProps) {
+  return (
+    <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+      <div
+        className="h-3 bg-violet-600 rounded-xl"
+        role="progressbar"
+        aria-label="Progresso de hábitos completados nesse dia"
+        aria-valuenow={props.progress}
+        style={{ width: `${props.progress}%` }}
+      ></div>
+    </div>
+  );
+}
