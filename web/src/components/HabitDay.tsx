3a4aa6815a0a27aab4ca6f7a9025e58b28fb1f34
Thomas Aleknovic <aleknovic70@gmail.com>
2023-01-25 14:36:55 -0300

    feat: Add progress bar and progress colors

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -13,9 +13,18 @@ function HabitDay({ completed, amount }: HabitProps) {
     <Popover.Root>
       <Popover.Trigger
         className={clsx(
-          "w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg ",
+          "w-10 h-10  border-2  border-zinc-800 bg-zinc-900 rounded-lg ",
           {
-            "bg-violet-500": completedPercentage >= 80,
+            "bg-violet-500 border-violet-400": completedPercentage >= 80,
+            "bg-violet-600 border-violet-500":
+              completedPercentage >= 60 && completedPercentage <= 80,
+            "bg-violet-700 border-violet-500":
+              completedPercentage >= 40 && completedPercentage <= 60,
+            "bg-violet-800 border-violet-600":
+              completedPercentage >= 20 && completedPercentage <= 40,
+            "bg-violet-900 border-violet-700":
+              completedPercentage > 20 && completedPercentage < 20,
+            "border-zinc-800 bg-zinc-900": completedPercentage === 0,
           }
         )}
       />
