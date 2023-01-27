4d4420a786f51a4af620f279f4a865264b936f2d
Thomas Aleknovic <aleknovic70@gmail.com>
2023-01-27 18:04:23 -0300

    fix: server host

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,7 +9,8 @@ app.register(appRoutes);
 
 const start = async () => {
   try {
-    await app.listen({ port: 3333 });
+    const url = await app.listen({ port: 3333, host: "0.0.0.0" });
+    console.log(`Server running on ${url}`);
   } catch (err) {
     app.log.error(err);
     process.exit(1);
