bf866e9acf302d802b4059774344fc39b1b37e65
Thomas Aleknovic <aleknovic70@gmail.com>
2023-01-28 11:13:47 -0300

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,10 +6,11 @@ const app = Fastify();
 
 app.register(cors);
 app.register(appRoutes);
+const port = process.env.PORT || 3333;
 
 const start = async () => {
   try {
-    const url = await app.listen({ port: 3333, host: "0.0.0.0" });
+    const url = await app.listen({ port: port, host: "0.0.0.0" });
     console.log(`Server running on ${url}`);
   } catch (err) {
     app.log.error(err);
